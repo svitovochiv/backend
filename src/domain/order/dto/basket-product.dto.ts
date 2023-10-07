72c386c5926f8b49cb2e2fe5fccc61fcd8a4071b
alkonl <oleh.ferents.work@gmail.com>
2023-10-07 20:39:06 +0300

    improved basket

--- src/domain/order/dto/basket-product.dto.ts
+++ src/domain/order/dto/basket-product.dto.ts
@@ -2,9 +2,11 @@ export class BasketProductDto {
   basketId: string;
   productId: string;
   count: number;
+  cost: number;
   constructor(data: BasketProductDto) {
     this.basketId = data.basketId;
     this.productId = data.productId;
     this.count = data.count;
+    this.cost = data.cost;
   }
 }
