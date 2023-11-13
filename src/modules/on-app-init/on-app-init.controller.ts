91a9f51854995d7921991a9884a34e39ae27c4e6
alkonl <oleh.ferents.work@gmail.com>
2023-11-13 22:19:36 +0200

    implemented ci/cd

--- src/modules/on-app-init/on-app-init.controller.ts
+++ src/modules/on-app-init/on-app-init.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { OnAppInitService } from './on-app-init.service';
 import { MockUsers } from '../../mock/mock-users';
+import { IsPublic } from '../auth';
 
 @Controller('on-app-init')
 export class OnAppInitController {
   constructor(private readonly onAppInitService: OnAppInitService) {}
 
   @Get()
+  @IsPublic()
   async onAppInit() {
     await this.onAppInitService.createUser(MockUsers);
   }
--- src/modules/on-app-init/on-app-init.module.ts
+++ src/modules/on-app-init/on-app-init.module.ts
@@ -2,10 +2,12 @@ import { UserModule } from '../user';
 import { Module } from '@nestjs/common';
 import { MockUsers } from '../../mock/mock-users';
 import { OnAppInitService } from './on-app-init.service';
+import { OnAppInitController } from './on-app-init.controller';
 
 @Module({
   imports: [UserModule],
   providers: [OnAppInitService],
+  controllers: [OnAppInitController],
 })
 export class OnAppInitModule {
   constructor(private readonly onAppInitService: OnAppInitService) {}
