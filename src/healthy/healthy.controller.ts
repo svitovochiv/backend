079775f3bf9bb1ab9b3baa34caadf07d9aa3160d
alkonl <oleh.ferents.work@gmail.com>
2023-11-13 17:55:04 +0200

    implemented ci/cd

--- src/healthy/healthy.controller.ts
+++ src/healthy/healthy.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { IsPublic } from '../modules';
 
 @Controller('healthy')
 export class HealthyController {
   @Get()
+  @IsPublic()
   getHealthy() {
     return { healthy: true };
   }
